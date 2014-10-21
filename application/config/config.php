2a3a4535740416bd1b568119ba79a371238665a0
Dr. Bizarre <oscar@drbizarre.net>
2014-10-20 23:26:22 -0700

    demo pagina de captura

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://localhost/lead-quest';
+$config['base_url']	= 'http://localhost/lead-quest/';
 
 /*
 |--------------------------------------------------------------------------
